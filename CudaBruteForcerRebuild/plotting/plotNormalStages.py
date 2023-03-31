ded1940c774092738cca73c16b45cf05cc50a217
TheLonelyPoire <thelonelypoire@gmail.com>
2023-03-31 00:26:12 -0400

    Implemented Command for Switching between Standard and Height Difference Plotting.

--- CudaBruteForcerRebuild/plotting/plotNormalStages.py
+++ CudaBruteForcerRebuild/plotting/plotNormalStages.py
@@ -6,9 +6,9 @@ import matplotlib
 from matplotlib import pyplot as plt
 
 
-def setupPlot(plotArray, sampleIdx, rParams : RangeParameters, nStages, pauseRate, usePara):
+def setupPlot(plotArray, sampleIdx, rParams : RangeParameters, nStages, pauseRate, usePara, colmap):
     implot = plt.imshow(plotArray[sampleIdx,:,:].transpose(), 
-                   cmap=colormap, interpolation='nearest', origin='upper',
+                   cmap=colmap, interpolation='nearest', origin='upper',
                    extent=rParams.getExtents(usePara),
                    vmin=-1 if usePara else 0, vmax=nStages-1)
     plt.colorbar()    
@@ -28,8 +28,9 @@ def setupPlot(plotArray, sampleIdx, rParams : RangeParameters, nStages, pauseRat
     return implot
 
 
-def update_image_plot(implot, img, pauseRate, title=''):
+def update_image_plot(implot, img, pauseRate : float, colmap : clrs.LinearSegmentedColormap, title=''):
     implot.set_array(img)
+    implot.set(cmap=colmap)
     plt.title(title)
     plt.pause(pauseRate)
     return implot
@@ -43,7 +44,6 @@ fileName = "normalStagesReached_3_30_22_3.bin"
 
 rangeParameters = getRangeParametersFromFile(getCorrespondingRangeParametersFilename(folderName + fileName))
 useParallelogram = False
-tryUseHeightDifference = False
 
 # Custom Range Parameters
 minNX = -0.213
@@ -59,40 +59,43 @@ useZXSum = True
 
 # rangeParameters = RangeParameters(minNX, maxNX, minNZorZXSum, maxNZorZXSum, minNY, maxNY, nSamplesNX, nSamplesNZ, nSamplesNY, useZXSum=useZXSum)
 
+foundHeightDifference = False
+
 if fileName.startswith("norm"):
     plotArr = getIntDataFromBinaryFile(fileName, folderName=folderName, nSamplesY=rangeParameters.nSamplesNY, nSamplesX=rangeParameters.nSamplesNX, nSamplesZ=rangeParameters.nSamplesNZ)
     
-    if tryUseHeightDifference:
-        try:
-            file, folder = getCorrespondingHeightDiffFilenameAndFolderPath(folderName + fileName)
-            heightDiffArr = getFloatDataFromBinaryFile(file, folder, rangeParameters.nSamplesNY, rangeParameters.nSamplesNX, rangeParameters.nSamplesNZ)
-            plotArr = plotArr.astype(float)
-            for i in range(rangeParameters.nSamplesNY):
-                for j in range(rangeParameters.nSamplesNX):
-                    for k in range(rangeParameters.nSamplesNZ):
-                        if(plotArr[i,j,k] == 8.0):
-                            plotArr[i,j,k] = 9 - min(0.01 * heightDiffArr[i,j,k], 1.0)
-            
-            print(heightDiffArr)
-        except:
-            print("Couldn't Locate Height Difference File at \'" + folder + file + "\' or encountered other error; Skipping")
+    try:
+        file, folder = getCorrespondingHeightDiffFilenameAndFolderPath(folderName + fileName)
+        heightDiffArr = getFloatDataFromBinaryFile(file, folder, rangeParameters.nSamplesNY, rangeParameters.nSamplesNX, rangeParameters.nSamplesNZ)
+        plotArrH = plotArr.astype(float)
+        for i in range(rangeParameters.nSamplesNY):
+            for j in range(rangeParameters.nSamplesNX):
+                for k in range(rangeParameters.nSamplesNZ):
+                    if(plotArr[i,j,k] == 8):
+                        plotArrH[i,j,k] = 9 - min(0.01 * heightDiffArr[i,j,k], 1.0)
+        
+        foundHeightDifference = True
+    except:
+        print("Couldn't Locate Height Difference File at \'" + folder + file + "\' or encountered other error; Skipping")
 else:
     plotArr = getFloatDataFromBinaryFile(fileName, folderName=folderName, nSamplesY=rangeParameters.nSamplesNY, nSamplesX=rangeParameters.nSamplesNX, nSamplesZ=rangeParameters.nSamplesNZ)
 
-# rangeParameters = RP_FINER_EXPANDED_RUN
-# plotArr = NS_FINER_EXPANDED_RUN
+# Load pre-existing run
+rangeParameters = RP_FINER_EXPANDED_RUN
+plotArr = NS_FINER_EXPANDED_RUN
+foundHeightDifference = False
 
 if(useParallelogram):
     plotArr = getDataAsParallelogram(plotArr)
+    plotArrH = getDataAsParallelogram(plotArrH)
 
 numStages = 10
 pauseRate = 0.05 
 
 if fileName.startswith("norm"):
-    if tryUseHeightDifference:
-        colormap = CM_HEIGHT_GRADIENT if not useParallelogram else CM_HEIGHT_GRADIENT_PARA
-    else:
-        colormap = CM_MARBLER if not useParallelogram else CM_MARBLER_PARA
+    colormap = CM_MARBLER if not useParallelogram else CM_MARBLER_PARA
+    if foundHeightDifference:
+        colormapH = CM_HEIGHT_GRADIENT if not useParallelogram else CM_HEIGHT_GRADIENT_PARA
 elif fileName.startswith("plat"):
     colormap = CM_HWR_BANDS 
 elif fileName.startswith("minUp"):
@@ -100,9 +103,11 @@ elif fileName.startswith("minUp"):
 else:
     colormap = CM_DEFAULT
 
-implot = setupPlot(plotArr, 0, rangeParameters, numStages, pauseRate, useParallelogram)
+implot = setupPlot(plotArr, 0, rangeParameters, numStages, pauseRate, useParallelogram, colormap)
 for ny in range(rangeParameters.nSamplesNY):
-    update_image_plot(implot, plotArr[ny,:,:].transpose(), pauseRate, 'nY = ' + str(round(ny * rangeParameters.getYStepSize() + rangeParameters.minNY,5)))
+    update_image_plot(implot, plotArr[ny,:,:].transpose(), pauseRate, colormap, 'nY = ' + str(round(ny * rangeParameters.getYStepSize() + rangeParameters.minNY,5)))
+
+useHeightDiff = False
 
 while(True):
     sample = input("Enter a sample for NY (0 indexed), a valid command (type 'help' for a list), or -1 to quit: ")
@@ -111,10 +116,16 @@ while(True):
         sample = int(sample)
 
         if sample >= rangeParameters.nSamplesNY:
-            print("Sample index is too high! Please enter a lower sample index!\n")
+            print("Sample index is too high! Please enter a lower sample index!")
             continue
+        
+        if not plt.get_fignums():
+            implot = setupPlot(plotArrH if useHeightDiff else plotArr, sample, rangeParameters, numStages, pauseRate, useParallelogram, colormapH if useHeightDiff else colormap)
 
-        update_image_plot(implot, plotArr[sample,:,:].transpose(), pauseRate, 'nY = ' + str(round(sample * rangeParameters.getYStepSize() + rangeParameters.minNY,5)))
+        if useHeightDiff:
+            update_image_plot(implot, plotArrH[sample,:,:].transpose(), pauseRate, colormapH, 'nY = ' + str(round(sample * rangeParameters.getYStepSize() + rangeParameters.minNY,5)))
+        else:
+            update_image_plot(implot, plotArr[sample,:,:].transpose(), pauseRate, colormap, 'nY = ' + str(round(sample * rangeParameters.getYStepSize() + rangeParameters.minNY,5)))
     
         plt.pause(pauseRate)
 
@@ -128,14 +139,22 @@ while(True):
         print('0,1,...  - Displays a single nY slice of the data based on the index entered. The input cannot exceed the number of nY samples - 1.')
         print('video    - Displays all nY samples in a continuous video, with the frame delay based on the speed of the plotter and the pause rate.')
         print('chpr <f> - Changes the pause rate to the input (pause rate is stored as a floating-point variable).')
+        print('thd      - Toggles whether or not the height difference is displayed (if loaded).')
         print('quit     - Quits the application (typing -1 will do the same).')
         print('help     - Prints this menu.')
 
         continue
 
     if sample == 'video':
+        if not plt.get_fignums():
+            implot = setupPlot(plotArrH if useHeightDiff else plotArr, 0, rangeParameters, numStages, pauseRate, useParallelogram, colormapH if useHeightDiff else colormap)
+
         for ny in range(rangeParameters.nSamplesNY):
-            update_image_plot(implot, plotArr[ny,:,:].transpose(), pauseRate, 'nY = ' + str(round(ny * rangeParameters.getYStepSize() + rangeParameters.minNY,5)))
+            if useHeightDiff:
+                update_image_plot(implot, plotArrH[ny,:,:].transpose(), pauseRate, colormapH, 'nY = ' + str(round(ny * rangeParameters.getYStepSize() + rangeParameters.minNY,5)))
+            else:
+                update_image_plot(implot, plotArr[ny,:,:].transpose(), pauseRate, colormap, 'nY = ' + str(round(ny * rangeParameters.getYStepSize() + rangeParameters.minNY,5))) 
+        
         continue
 
 
@@ -148,6 +167,15 @@ while(True):
                 raise ValueError("Pause value must be positive.")
         except:
             print("Invalid pause rate entered; could not change pause rate.")
+
+        continue
+
+    if sample == 'thd':
+        if foundHeightDifference:
+            useHeightDiff = not useHeightDiff
+            print("Height difference plotting is now " + ("enabled" if useHeightDiff else "disabled") + ".")
+        else:
+            print("Height difference file couldn't be loaded; please ensure height difference file is present and named correctly.")
 
         continue
     
